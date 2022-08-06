f3cc1a9b5eca613c6daa24b676240c0bedd744aa
Quinton Outten <qoutten@quintons-mbp.lan>
2022-08-06 16:32:08 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Quintono/doesntmatter
+
+go 1.19
---
+++ src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let foo = Some(5);
+
+    if let Some(value) = foo {
+
+    }
+
+    match foo {
+        Some(value) => {
+        },
+        None => {}
+    }
+
+    foo.map(|x| {
+    });
+
+    foo.filter(|x| x < &10);
+}
+
